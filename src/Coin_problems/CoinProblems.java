97246b0de665996b633bfd5ca8e4c52989c40b8d
Abdulrahman <bou3shandle@gmail.com>
2017-08-14 08:49:32 +0300

    Adding coin problems class without any implementation

---
+++ src/Coin_problems/CoinProblems.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Coin_problems;
+
+/**
+ *
+ * @author Abdulrahman
+ */
+public class CoinProblems {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
