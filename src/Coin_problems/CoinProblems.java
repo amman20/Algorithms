2087cd50b4fd4b0b509dea8bfd193ae61ccc6037
Abdulrahman <bou3shandle@gmail.com>
2017-08-20 22:41:58 +0300

    Adding Collecting Coins problem.

--- src/Coin_problems/CoinProblems.java
+++ src/Coin_problems/CoinProblems.java
@@ -19,13 +19,23 @@ public class CoinProblems {
     //the input for changeMaking
     static int[] changes = {0, 1, 3, 4};
     //this is the input for getting maximum value if move right and down only.
-    static int[][] board = {};
+    static int[][] board = {
+        //  1   2   3   4   5   6
+        {0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 1, 0, 1, 0, 0},
+        {0, 0, 0, 0, 1, 0, 1},
+        {0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 1, 0, 0, 1},
+        {0, 1, 0, 0, 0, 1, 0}
+    };
 
     public static void main(String[] args) {
         //Geting max money for not adjacent C
-//        System.out.println(MaxMoneyNotAdjacent());
-        System.out.println(ChangeMaking(6));
-
+        System.out.println("Maximum Money Nonadjacent coins: "+MaxMoneyNotAdjacent());
+        //change making problem
+        System.out.println("Change Making: "+ChangeMaking(6));
+        //Coins Collecing Problem.
+        System.out.println("Collecing Coins: "+CoinCollecing());
 
     }
 
@@ -62,6 +72,28 @@ public class CoinProblems {
         }
         return F[n];
         //time complexity is O(nm)
+    }
+
+    public static int CoinCollecing() {
+        int n = board.length, m = board[0].length;
+        int[][] F = new int[n][m];
+
+        //F[1,1] = Board[1,1]
+        F[1][1] = board[1][1];
+        //initialize all the row as cummulative array since it depends on only on the left cell value.
+        for (int i = 2; i < m; i++) {
+            F[1][i] = F[1][i - 1] + board[1][i];
+        }
+
+        for (int i = 1; i < n; i++) {
+            //the first value in the row depends only on the above value only, so start with column 2 in the inner loop.
+            F[i][1] = F[i - 1][1] + board[i][1];
+            for (int j = 2; j < m; j++) {
+                //the value of cell F[i][j] depends on either the value of (the above cell F[i-1][j] or the left cell F[i][j-1] ) plus its value.
+                F[i][j] = Math.max(F[i - 1][j], F[i][j - 1]) + board[i][j];
+            }
+        }
+        return F[n - 1][m - 1];
     }
 
 }
